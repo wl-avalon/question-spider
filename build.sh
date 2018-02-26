e42cdb786d05e6b341032a3117ed0fdca1db6ecc
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-02-26 12:29:39 +0800

    提交代码

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-MODULE_NAME="question_spider"
+MODULE_NAME="question-spider"
 MODULE_DIR_PATH="output/application/$MODULE_NAME/modules"
-WEB_DIR_PATH="output/application/$MODULE_NAME/web"
+#WEB_DIR_PATH="output/application/$MODULE_NAME/web"
 CONSOLE_DIR_PATH="output/application/$MODULE_NAME/console"
 rm -rf output
 mkdir -p ${MODULE_DIR_PATH}
-mkdir -p ${WEB_DIR_PATH}
+#mkdir -p ${WEB_DIR_PATH}
+mkdir -p ${CONSOLE_DIR_PATH}
 cp -r apis commands services components config constants models scripts Module.php ${MODULE_DIR_PATH}
 cp -r console/* ${CONSOLE_DIR_PATH}
+#cp -r web/* ${WEB_DIR_PATH}
 cd output
 find ./ -name .git -exec rm -rf {} \;
 tar cvzf ${MODULE_NAME}.tar.gz application
