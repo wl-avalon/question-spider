1b786e96e6551ceb2f78d5ae156d6927d048c7bb
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-02-27 18:06:31 +0800

    提交代码

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 MODULE_NAME="question-spider"
 MODULE_DIR_PATH="output/application/$MODULE_NAME/modules"
-LOG_DIR_PATH="output/application/$MODULE_NAME/modules/logs"
 WEB_DIR_PATH="output/application/$MODULE_NAME/web"
 CONSOLE_DIR_PATH="output/application/$MODULE_NAME/console"
 rm -rf output
 mkdir -p ${MODULE_DIR_PATH}
-mkdir -p ${LOG_DIR_PATH}
 mkdir -p ${WEB_DIR_PATH}
 mkdir -p ${CONSOLE_DIR_PATH}
 cp -r actions apis commands controllers services components config constants models scripts Module.php ${MODULE_DIR_PATH}
